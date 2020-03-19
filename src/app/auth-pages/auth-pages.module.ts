6b16e446a99c1b7e1131b3b3f9009ff8de73deaf
bengeos <george.beng@gmail.com>
2020-03-19 16:05:11 +0300

    update chanhes

--- src/app/auth-pages/auth-pages.module.ts
+++ src/app/auth-pages/auth-pages.module.ts
@@ -3,12 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { AuthPagesRoutingModule } from './auth-pages-routing.module';
 import { LoginComponent } from './login/login.component';
+import {ComponentsModule} from '../components/components.module';
+import {FormsModule} from '@angular/forms';
+import {
+  MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatSelectModule,
+  MatTooltipModule
+} from '@angular/material';
+import {NgxLoadingModule} from 'ngx-loading';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AuthPagesRoutingModule
+    AuthPagesRoutingModule,
+    ComponentsModule,
+    FormsModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+    NgxLoadingModule.forRoot({})
   ]
 })
 export class AuthPagesModule { }
