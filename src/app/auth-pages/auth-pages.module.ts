48c0729b05a9a453bf297d4d826f8565bf67414a
bengeos <george.beng@gmail.com>
2020-03-19 15:03:43 +0300

    update changes

---
+++ src/app/auth-pages/auth-pages.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthPagesRoutingModule } from './auth-pages-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthPagesRoutingModule
+  ]
+})
+export class AuthPagesModule { }
