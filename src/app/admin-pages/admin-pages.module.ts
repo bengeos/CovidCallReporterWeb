5f46d3d2e56e6b04ca637dc05789a6ea22ecbec4
bengeos <george.beng@gmail.com>
2020-03-19 15:06:50 +0300

    update changes

--- src/app/admin-pages/admin-pages.module.ts
+++ src/app/admin-pages/admin-pages.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminPagesRoutingModule } from './admin-pages-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent, UserComponent],
   imports: [
     CommonModule,
     AdminPagesRoutingModule
