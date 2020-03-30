ecc706d1df5c671427fa83ff438290127d65c0fa
bengeos <george.beng@gmail.com>
2020-03-30 23:08:27 +0300

    update root url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    // root_url: 'http://covid-response-app-prod.us-east-2.elasticbeanstalk.com/',
+    root_url: 'http://covid-response-app-prod.us-east-2.elasticbeanstalk.com/',
     // root_url: 'https://api.covidfollowupportal.negarit.net/',
-    root_url: 'http://127.0.0.1:8000/',
+    // root_url: 'http://127.0.0.1:8000/',
 };
