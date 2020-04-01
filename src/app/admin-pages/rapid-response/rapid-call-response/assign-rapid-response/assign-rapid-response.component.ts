e33fd8c7cde57981b581571c0f655c783f05fa7c
bengeos <george.beng@gmail.com>
2020-04-01 09:56:57 +0300

    update changes

---
+++ src/app/admin-pages/rapid-response/rapid-call-response/assign-rapid-response/assign-rapid-response.component.ts
@@ -0,0 +1,41 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {AssignedCallReport, CallReport} from '../../../call-report/call-reports.objects';
+import {Team} from '../../../team-contacts/team-contacts.objects';
+import {TeamsService} from '../../../services/teams.service';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+    selector: 'app-assign-rapid-response',
+    templateUrl: './assign-rapid-response.component.html',
+    styleUrls: ['./assign-rapid-response.component.scss']
+})
+export class AssignRapidResponseComponent implements OnInit {
+    public selectedCallReport = new CallReport();
+    public newAssignCallReport = new AssignedCallReport();
+    public teams: Team[] = [];
+    public loading = false;
+
+    constructor(private teamsService: TeamsService, public dialogRef: MatDialogRef<AssignRapidResponseComponent>,
+                @Inject(MAT_DIALOG_DATA) new_data: CallReport) {
+        this.selectedCallReport = new_data;
+        this.newAssignCallReport.call_report_id = this.selectedCallReport.id;
+    }
+
+    ngOnInit() {
+        this.teamsService.getTeamsList();
+        this.teamsService.TeamsListEmitter.subscribe(
+            data => {
+                this.teams = data
+            }
+        );
+    }
+
+    public assignCallReport() {
+        this.dialogRef.close(this.newAssignCallReport);
+    }
+
+    public cancel() {
+        this.dialogRef.close();
+    }
+
+}
--- src/app/admin-pages/rapid-response/rapid-call-response/rapid-call-response.component.ts
+++ src/app/admin-pages/rapid-response/rapid-call-response/rapid-call-response.component.ts
@@ -5,77 +5,115 @@ import {SwalMessagesService} from '../../services/swal-messages.service';
 import {CallReportsService} from '../../services/call-reports.service';
 import {NewReportComponent} from '../../report/call-reports/new-report/new-report.component';
 import {UpdateRapidCallResponseComponent} from './update-rapid-call-response/update-rapid-call-response.component';
+import {UpdateAssignedFollowupReportComponent} from "../../follow-up/followup-call-reports/update-assigned-followup-report/update-assigned-followup-report.component";
+import {AssignFollowupReportComponent} from "../../follow-up/followup-call-reports/assign-followup-report/assign-followup-report.component";
+import {AssignRapidResponseComponent} from "./assign-rapid-response/assign-rapid-response.component";
 
 @Component({
-  selector: 'app-rapid-call-response',
-  templateUrl: './rapid-call-response.component.html',
-  styleUrls: ['./rapid-call-response.component.scss']
+    selector: 'app-rapid-call-response',
+    templateUrl: './rapid-call-response.component.html',
+    styleUrls: ['./rapid-call-response.component.scss']
 })
 export class RapidCallResponseComponent implements OnInit, OnChanges {
-  @Input() allCallReports;
-  public paginated_call_report = new PaginatedCallReport();
-  public pageSizeOptions: number[] = [5, 10, 15, 25, 50, 100, 500, 1000];
-  public loading = false;
+    @Input() allCallReports;
+    public paginated_call_report = new PaginatedCallReport();
+    public pageSizeOptions: number[] = [5, 10, 15, 25, 50, 100, 500, 1000];
+    public loading = false;
 
-  constructor(private dialog: MatDialog, private responseMessageService: SwalMessagesService,
-              private callReportsService: CallReportsService) {
-  }
-
-  ngOnInit() {
-    this.updateCallReportsComponent();
-    this.callReportsService.PaginatedRapidCallReportEmitter.subscribe(
-        data => {
-          this.paginated_call_report = data;
-          this.loading = false;
-        }
-    );
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('changes', changes);
-    this.updateCallReportsComponent();
-  }
-
-  public updateCallReportsComponent() {
-    this.loading = true;
-    if (this.allCallReports) {
-      this.callReportsService.getNewRapidCallReports();
-    } else {
-      this.callReportsService.getAssignedRapidCallReports();
+    constructor(private dialog: MatDialog, private responseMessageService: SwalMessagesService,
+                private callReportsService: CallReportsService) {
     }
-  }
 
-  public updatePaginatedCallReportData(event: any) {
-    this.loading = true;
-    const page_num = event.pageIndex + 1;
-    const paginate_size = event.pageSize;
-    this.callReportsService.getPaginatedRegionsData(this.paginated_call_report.path + '?page='
-        + page_num + '&PAGINATE_SIZE=' + paginate_size);
-  }
-
-  public updateRapidCallReport(report: CallReport): void {
-    const dialogConfig = new MatDialogConfig();
-    dialogConfig.disableClose = false;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = '1200px';
-    dialogConfig.data = report;
-    const dialogRef = this.dialog.open(UpdateRapidCallResponseComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        console.log('NEW-Report', result);
-        this.loading = true;
-        this.callReportsService.updateCallReportGroup(result).subscribe(
-            succes => {
-              this.loading = false;
-              this.responseMessageService.showNotification(2, 'top', 'right', 'Report Updated Successfully');
-              this.updateCallReportsComponent();
-            },
-            failed => {
-              this.loading = false;
-              this.responseMessageService.displayErrorResponseMessage(failed);
+    ngOnInit() {
+        this.updateCallReportsComponent();
+        this.callReportsService.PaginatedRapidCallReportEmitter.subscribe(
+            data => {
+                this.paginated_call_report = data;
+                this.loading = false;
             }
         );
-      }
-    });
-  }
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log('changes', changes);
+        this.updateCallReportsComponent();
+    }
+
+    public updateCallReportsComponent() {
+        this.loading = true;
+        if (this.allCallReports) {
+            this.callReportsService.getNewRapidCallReports();
+        } else {
+            this.callReportsService.getAssignedRapidCallReports();
+        }
+    }
+
+    public updatePaginatedCallReportData(event: any) {
+        this.loading = true;
+        const page_num = event.pageIndex + 1;
+        const paginate_size = event.pageSize;
+        this.callReportsService.getPaginatedRegionsData(this.paginated_call_report.path + '?page='
+            + page_num + '&PAGINATE_SIZE=' + paginate_size);
+    }
+
+    public updateRapidCallReport(report: CallReport): void {
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.disableClose = false;
+        dialogConfig.autoFocus = true;
+        dialogConfig.width = '1200px';
+        dialogConfig.data = report;
+        const dialogRef = this.dialog.open(UpdateRapidCallResponseComponent, dialogConfig);
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                console.log('NEW-Report', result);
+                this.loading = true;
+                this.callReportsService.updateCallReportGroup(result).subscribe(
+                    succes => {
+                        this.loading = false;
+                        this.responseMessageService.showNotification(2, 'top', 'right', 'Report Updated Successfully');
+                        this.updateCallReportsComponent();
+                    },
+                    failed => {
+                        this.loading = false;
+                        this.responseMessageService.displayErrorResponseMessage(failed);
+                    }
+                );
+            }
+        });
+    }
+
+    public assignRapidResponseReport(report: CallReport) {
+        const dialogConfig = new MatDialogConfig();
+        dialogConfig.disableClose = false;
+        dialogConfig.autoFocus = true;
+        dialogConfig.width = '800px';
+        dialogConfig.data = report;
+        if (this.allCallReports) {
+            const dialogRef = this.dialog.open(AssignRapidResponseComponent, dialogConfig);
+            dialogRef.afterClosed().subscribe(result => {
+                if (result) {
+                    console.log('Update Assigned-RRT-Report', result);
+                }
+            });
+        } else {
+            const dialogRef = this.dialog.open(AssignRapidResponseComponent, dialogConfig);
+            dialogRef.afterClosed().subscribe(result => {
+                if (result) {
+                    console.log('Assign-RRT-Report', result);
+                    this.loading = true;
+                    this.callReportsService.assignCallReportForRapidResponseTeam(result).subscribe(
+                        succes => {
+                            this.loading = false;
+                            this.responseMessageService.showNotification(2, 'top', 'right', 'Call-Report Assigned Successfully');
+                            this.updateCallReportsComponent();
+                        },
+                        failed => {
+                            this.loading = false;
+                            this.responseMessageService.displayErrorResponseMessage(failed);
+                        }
+                    );
+                }
+            });
+        }
+    }
 }
